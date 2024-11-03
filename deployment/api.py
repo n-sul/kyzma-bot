2c813653c62ad9933213424fbcaacd088ec3f00c
nazar-sulima <nazarukrainian47@gmail.com>
2024-11-03 13:17:33 +0000

    2

--- deployment/api.py
+++ deployment/api.py
@@ -4,16 +4,12 @@ from bot import bot
 
 app = FastAPI()
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Welcome to the bot!")
-
 @app.post("/webhook")
 async def webhook(request: Request):
-    update = request.json()
+    update = await request.json()
     bot.process_new_updates([telebot.types.Update.de_json(update)])
     return {"status": "ok"}
 
 @app.get("/")
-async def read_root():
+def read_root():
     return {"message": "Hello, this is your Telegram bot!"}
